17ae29b62f67aeb5a9b4690fcfebb8c141275392
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-10-07 17:32:08 +0000

    new way to write the code

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,17 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
+for (let list of packingList) {
+  console.log(list);
+}
+
+/* same as :
+
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+*/
